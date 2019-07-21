c383263f8c5ba55965ae080db9ee4cd297f57355
uliaaan <markovopg@gmail.com>
2019-07-21 20:17:19 +0300

    Fix port

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const app = require('express')();
 const http = require('http').Server(app);
 const dotenv = require('dotenv');
 dotenv.config();
+require('./compute/telegram');
 
-const telegram = require('./compute/telegram');
-
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3020;
 
 http.listen(port, () => console.log(`Server start on porn ${port}`));
